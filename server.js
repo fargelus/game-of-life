597418fd021336b0f18cad3472bb2cf0cdf1dc76
fargelus <ddraudred@gmail.com>
2017-09-26 10:27:42 +0300

    Connected to redis

--- server.js
+++ server.js
@@ -2,6 +2,13 @@ const express = require('express');
 const path = require('path');
 const redis = require('redis');
 
+// redis client
+let client = redis.createClient();
+
+client.on('connect', function () {
+  console.log('Redis connected...');
+});
+
 // port
 const port = 3000;
 
@@ -12,7 +19,7 @@ app.get('/', function (req, res) {
   res.sendFile(path.join(__dirname + '/index.html'));
 });
 
-app.use(express.static('.'));
+app.use(express.static(path.join(__dirname + '/src/js/')));
 
 app.listen(port, function () {
   console.log('Server starts at ' + port);
